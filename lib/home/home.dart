734e8a8bc823b79fbc3eb8448eafb7d623facd07
NanduNKnandu <nknandu501@gmail.com>
2024-05-18 13:18:13 +0530

    mockkk

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,17 +1,1 @@
-import 'package:flutter/material.dart';
 
-class homee extends StatefulWidget {
-  const homee({super.key});
-
-  @override
-  State<homee> createState() => _homeeState();
-}
-
-class _homeeState extends State<homee> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("home")),
-    );
-  }
-}
